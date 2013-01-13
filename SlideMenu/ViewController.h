5cd230f8b71e4e8575816fabcfae71bd9154d5ce
Kyle Begeman <Kyle@Kyles-MacBook-Pro.local>
2013-01-13 14:59:41 -0500

    Initial Commit

---
+++ SlideMenu/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  SlideMenu
+//
+//  Created by Kyle Begeman on 1/13/13.
+//  Copyright (c) 2013 Indee Box LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ SlideMenu/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  SlideMenu
+//
+//  Created by Kyle Begeman on 1/13/13.
+//  Copyright (c) 2013 Indee Box LLC. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
